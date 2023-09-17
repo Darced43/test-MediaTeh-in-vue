b1e067ef7a052b8e2a77d5726b1b28f77abeec8b
Кирилл Лапихин <k19-92@mail.ru>
2023-09-17 11:08:17 +0300

    edit route end config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/test-MediaTeh-in-vue'
 })
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/test-MediaTeh-in-vue/' 
-  : '/',
-}
